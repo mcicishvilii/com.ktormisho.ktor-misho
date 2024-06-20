70bbd9e95d268ee5127414c6889c1cde9b045979
mcicishvilii <mcicishvilii@gmail.com>
2024-06-20 12:09:28 +0400

    changed athe port soimething

--- src/main/kotlin/com/ktormisho/Application.kt
+++ src/main/kotlin/com/ktormisho/Application.kt
@@ -3,8 +3,12 @@ package com.ktormisho
 import com.ktormisho.plugins.*
 import io.ktor.server.application.*
 
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
+    embeddedServer(Netty, port = port, module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
